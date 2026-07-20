073617fb45d63d30c6d511ca22769bb4324072f6
Manoj Sharma <your.email@example.com>
2026-07-20 13:04:23 +0530

    feat : implement permanent file storage for journal entries

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+
+use std::fs::OpenOptions;
 use std::io::{self,Write};
+
 fn main () {
     println!("welcome to your cli journal ");
-    print!("write our entry");
+    print!("write our entry : ");
     io::stdout().flush().unwrap();
 
 let mut input = String::new();
@@ -9,6 +12,13 @@ io::stdin()
 .read_line(&mut input)
 .expect("failed to read line");
 let cleaned_input = input.trim();
-println!("\nSaved text : \"{}\"",cleaned_input);
+
+let mut file = OpenOptions::new()
+.append(true)
+.create(true)
+.open("journal.txt")
+.expect("failed to open or create file");
+writeln!(file,"{}",cleaned_input).expect("failed ot write to file");
+println!("\n success ! your entry has been saved to journal.txt ");
 
 }
